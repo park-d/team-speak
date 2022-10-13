12b321792a30c8a59578b1e2cf8ea0f9046272b5
Roy <loganroyjr4@gmail.com>
2022-10-13 15:22:04 -0400

    created user login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -57,6 +57,64 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+      const newUser = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+      });
+  
+      req.session.save(() => {
+        req.session.userId = newUser.id;
+        req.session.loggedIn = true;
+  
+        res.status(200).json(newUser);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+router.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+  
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await user.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+  
+        res
+          .status(200)
+          .json({user: user , message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
 
 
