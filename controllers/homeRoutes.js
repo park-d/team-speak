30f363d029a93acdefd1e2829d41a6292e858efd
park-d <54589582+park-d@users.noreply.github.com>
2022-10-17 12:45:25 -0500

    added in the news data to display in tabs instead of one long list, and other small changes in css and readme.md

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
 
 // login get route, if the user is logged in, redirect the page to the homepage, if not, then render the login page
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
+    if(req.session.loggedIn) {
         res.redirect('/');
         return;
     }
@@ -22,7 +22,7 @@ router.get('/login', (req, res) => {
 
 // signup get route, if the user is logged in, redirect the page to the homepage, if not, then render the sign-up page
 router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
+    if(req.session.loggedIn) {
         res.redirect('/userDashboard');
         return;
     }
@@ -42,8 +42,8 @@ router.get('/teamDashboard', async (req, res) => {
             type: QueryTypes.SELECT
         });
         console.log(posts);
-        res.render('teamDashboard', { posts });
-    } catch (err) {
+        res.render('teamDashboard', {posts});
+    } catch(err) {
         res.status(500).json(err);
     }
 
@@ -57,8 +57,8 @@ router.get('/posts/:id', async (req, res) => {
         const post = currentPosts.get({plain: true});
         const currentNews = await News.findByPk(post.article_id);
         const news = currentNews.get({plain: true});
-        var postData = {...post, ...news}; 
-        console.log(postData)
+        var postData = {...post, ...news};
+        console.log(postData);
         res.render('comments', {postData});
     } catch(err) {
         res.status(500).json(err);
@@ -102,15 +102,83 @@ router.get('/userDashboard', async (req, res) => {
             const allNews = await News.findAll({
                 where: {
                     category: categoryParams,
-                    article_id: [130, 132, 2000]
+                    article_id: [130, 132, 2000, 893]
                 }
-            }
-            );
-
+            });
             //because of the way sequelize returns data, we have to trim unwanted formatting (nested objects) with plain: true
             const article = allNews.map((news) => news.get({plain: true}));
+
+            const sportsArticles = [];
+            const politicsArticles = [];
+            const wellnessArticles = [];
+            const travelArticles = [];
+            const stylebeautyArticles = [];
+            const healthylivingArticles = [];
+            const parentingArticles = [];
+            const queervoicesArticles = [];
+            const fooddrinkArticles = [];
+            const businessArticles = [];
+            const comedyArticles = [];
+            const blackvoicesArticles = [];
+            const homelivingArticles = [];
+            const parentsArticles = [];
+            const entertainmentArticles = [];
+
+            for(let i = 0; i < article.length; i++) {
+                switch(article[i].category) {
+                    case "SPORTS":
+                        sportsArticles.push(article[i]);
+                        break;
+                    case "POLITICS":
+                        politicsArticles.push(article[i]);
+                        break;
+                    case "WELLNESS":
+                        wellnessArticles.push(article[i]);
+                        break;
+                    case "TRAVEL":
+                        travelArticles.push(article[i]);
+                        break;
+                    case "STYLE & BEAUTY":
+                        stylebeautyArticles.push(article[i]);
+                        break;
+                    case "HEALTHY LIVING":
+                        healthylivingArticles.push(article[i]);
+                        break;
+                    case "PARENTING":
+                        parentingArticles.push(article[i]);
+                        break;
+                    case "QUEER VOICES":
+                        queervoicesArticles.push(article[i]);
+                        break;
+                    case "FOOD & DRINK":
+                        fooddrinkArticles.push(article[i]);
+                        break;
+                    case "BUSINESS":
+                        businessArticles.push(article[i]);
+                        break;
+                    case "COMEDY":
+                        comedyArticles.push(article[i]);
+                        break;
+                    case "BLACK VOICES":
+                       blackvoicesArticles.push(article[i]);
+                        break;
+                    case "HOME & LIVING":
+                        homelivingArticles.push(article[i]);
+                        break;
+                    case "PARENTS":
+                        parentsArticles.push(article[i]);
+                        break;
+                    case "ENTERTAINMENT":
+                        entertainmentArticles.push(article[i]);
+                        break;
+                    default:
+                        break;
+                }
+            }
             // rendering the userDashboard handlebars view and passing the reformatted data to it
-            res.render("userDashboard", {article});
+            res.render("userDashboard", {sportsArticles, politicsArticles, wellnessArticles, travelArticles, stylebeautyArticles, healthylivingArticles, parentingArticles, queervoicesArticles, fooddrinkArticles, businessArticles, comedyArticles, blackvoicesArticles, homelivingArticles, parentsArticles, entertainmentArticles});
+            // res.render("test", {sportsArticles, politicsArticles, wellnessArticles, travelArticles, stylebeautyArticles, healthylivingArticles, parentingArticles, queervoicesArticles, fooddrinkArticles, businessArticles, comedyArticles, blackvoicesArticles, homelivingArticles, parentsArticles, entertainmentArticles});
+
 
         }
     } catch(err) {
