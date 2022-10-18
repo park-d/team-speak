f9ccc3615824cbd9c2c7812470d69116f7399393
Jackson Myhre <jack421myhre@gmail.com>
2022-10-17 20:03:51 -0500

    Removed add team delete button, added RD to the landing page metrics as well as the call-to-action signup/register buttons. Fixed register company link on landing page.

--- public/js/teamList.js
+++ public/js/teamList.js
@@ -1,24 +1,12 @@
 const teamListEl = $('.company-team-signup');
 const addTeamBtn = $('.add-team-button');
 
-// Can probably delete
-teamListEl.on('change', () => {
-    if (teamListEl.val() === '') {
-        addTeamBtn.disabled = true;
-    } else {
-        addTeamBtn.disabled = false;
-    }
-});
-
-
-
-
 addTeamBtn.on('click', (e) => {
     e.preventDefault();
     console.log('clicked');
 
     $('#team-list').append(
-        '<input type="text" placeholder="Team Name" class="form-input company-team-signup">\n<div class="delete-team"><img class="delete-button" src="./images/icons8-cancel-100.png"/></div>'
+        '<input type="text" placeholder="Team Name" class="form-input company-team-signup">'
     );
 
 
